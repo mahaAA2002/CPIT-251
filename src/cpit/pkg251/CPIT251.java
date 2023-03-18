993c029f47c3a8028a0effdfd160f1040d9dfdd1
Maha Alzahrani <maom13579001@gmail.com>
2023-03-18 19:05:20 +0300

    First Commit

---
+++ src/cpit/pkg251/CPIT251.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cpit.pkg251;
+
+/**
+ *
+ * @author maom1
+ */
+public class CPIT251 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
